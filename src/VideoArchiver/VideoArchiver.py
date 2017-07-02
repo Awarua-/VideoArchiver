efc7e91a4b76cb47ed6fbb68e72c6c2e6fd87387
Dion Woolley <woolley.dion@gmail.com>
2017-07-02 22:32:13 +1200

    move directories of project source

--- src/VideoArchiver/VideoArchiver.py
+++ src/VideoArchiver/VideoArchiver.py
@@ -12,16 +12,25 @@ H264_CHECK_STRING = "h264"
 def read(input):
     click.echo('transcoding file: {}'.format(input.name))
     session = Transcode(input.name)
-    session.transcode()
+    session.run()
 
 
 class Transcode(object):
 
-    def __init__(self, path):
+    def __init__(self, path='./placeholder'):
+        """This initialises the Transcode session."""
         self.path = path
+        self.__create_temp_file()
 
-    def transcode(self):
-        command = ("ffprobe -show_entries stream=codec_name " +
+    def run(self):
+        if self.__check_if_file_valid() == 0:
+            if self.__transcode() == 0:
+                self.__move_file()
+
+        self.__close_temp_file()
+
+    def __check_if_file_valid(self):
+        command = ("ffprobe -v quiet -show_entries stream=codec_name " +
                    "-select_streams v:0 -of " +
                    "default=noprint_wrappers=1").split(' ')
 
@@ -37,44 +46,48 @@ class Transcode(object):
                 break
             if output:
                 out = out + output
-        rc = process.poll()
-
         print(out)
         if H264_CHECK_STRING not in out:
             click.echo("File was not encoded with {}, but instead was {}"
                        .format(H264_CHECK_STRING, out))
-            return
+            return -1
+        return process.poll()
 
-        tfile = tempfile.NamedTemporaryFile(mode='w')
-
-        command = "ffmpeg -c:v h264_cuvid -i ".split(' ')
+    def __transcode(self):
+        command = "ffmpeg -c:v h264_cuvid -i".split(' ')
         command.append(os.path.realpath(self.path))
-        command + " -map 0 -c copy -c:v hevc_nvenc -preset slow ".split(' ')
-        command.append(os.path.realpath(tfile.name))
-
+        command += "-map 0 -c copy -c:v hevc_nvenc -preset slow".split(' ')
+        command.append(os.path.realpath(self.tfile.name))
+        command.append("-y")
         transcodeProcess = subprocess.Popen(command, stdout=subprocess.PIPE,
                                             stderr=subprocess.STDOUT,
                                             encoding='utf-8')
 
         out = ""
         while True:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
+            output = transcodeProcess.stdout.readline()
+            if output == '' and transcodeProcess.poll() is not None:
                 break
             if output:
-                print(output)
-                out = out + output
-        rc = process.poll()
+                out += output
+        rc = transcodeProcess.poll()
 
         if rc != 0:
             click.echo("Failed to transcode file:\n{}"
                        .format(transcodeProcess.stdout))
-            tfile.close()
-            return
+            return -1
+        return 0
 
-        #shutil.copyfile(os.path.realpath(tfile.name), os.path.realpath(self.path))
+    def __move_file(self):
+        shutil.copyfile(os.path.realpath(self.tfile.name),
+                        os.path.realpath(self.path))
 
-        tfile.close()
+    def __close_temp_file(self):
+        self.tfile.close()
+
+    def __create_temp_file(self):
+        self.tfile = tempfile.NamedTemporaryFile(mode='w')
+        self.tfile.name += '.mkv'
 
 if __name__ == "__main__":
     file = read()
